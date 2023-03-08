89952bb76ede8ec33715187d04b9ba5c8e27d75c
Daeyeol Ryu <dy@yoobato.com>
2023-03-08 14:51:09 +0900

    Fetch Managed Directories

--- crowd.py
+++ crowd.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import requests
 
 
@@ -16,3 +17,20 @@ class Crowd:
         # Format of Crowd Group ID and User ID are (Directory ID)-(Group Name or User Name encoded with base64)
         base64_encoded_group_name = base64.b64encode(group_name.encode('utf-8')).decode('utf-8')
         return str(directory_id) + '-' + base64_encoded_group_name
+    
+    
+    def get_managed_directories(self) -> list:
+        results = list()
+        start_index = 0
+
+        while(True):
+            # Fetch all Crowd directories by 100.
+            response = self._session.get(f'{self._admin_api_base_url}/directory/managed?limit=100&start={start_index}').json()
+            results.extend(list(response['values']))
+
+            if response['isLastPage'] == True:
+                break
+            else:
+                start_index = int(response['start']) + int(response['size'])
+        
+        return results
